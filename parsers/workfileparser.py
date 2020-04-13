cd15707782b1bc159e039cd342e9181bcdba473d
acpmasquerade <acpmasquerade@gmail.com>
2020-04-13 16:13:28 +0545

    working version of a workfile parser with the new spec

--- parsers/workfileparser.py
+++ parsers/workfileparser.py
@@ -32,7 +32,8 @@ class Project:
 		self.desc = desc
 
 	def serialize(self):
-		dump = dict(namespace=self.namespace, name=self.name, users=self.users, groups=self.groups, milestones=[], deadline=self.deadline, desc="\n".join(self.desc))
+		dump = dict(namespace=self.namespace, name=self.name, users=self.users, groups=self.groups, deadline=self.deadline, desc="\n".join(self.desc))
+		dump['milestones'] = []
 		for m in self.milestones:
 			dump['milestones'].append(m.serialize())
 
@@ -47,14 +48,14 @@ class Milestone:
 	desc = []
 	tasks = []
 
-	def __init__(self, name, users=[], groups=[], deadline=None, priority=None, desc=[], tasks=[]):
+	def __init__(self, name):
 		self.name = name
-		self.users = users
-		self.groups = groups
-		self.deadline = deadline
-		self.desc = desc
-		self.priority = priority
-		self.tasks = tasks
+		self.users = []
+		self.groups = []
+		self.deadline = None
+		self.desc = []
+		self.priority = "Normal"
+		self.tasks = []
 
 	def serialize(self):
 		dump = dict(name=self.name, users=self.users, groups=self.groups, deadline=self.deadline, priority=self.priority, desc="\n".join(self.desc), tasks=[])
@@ -72,24 +73,22 @@ class Task:
 	subtasks = []
 	desc = []
 
-	def __init__(self, name, users=[], groups=[], deadline=None, priority=None, subtasks=[]):
+	def __init__(self, name):
 		self.name = name
-		self.users = users
-		self.groups = groups
-		self.deadline = deadline
-		self.priority = priority
-		self.subtasks = subtasks
+		self.users = []
+		self.groups = []
+		self.deadline = None
+		self.priority = "Normal"
+		self.subtasks = []
 
 	def serialize(self):
-		dump = dict(name=self.name, users=self.users, groups=self.groups, deadline=self.deadline, priority=self.priority, desc="\n".join(self.desc), subtasks=[])
-		if self.subtasks:
-			for t in self.subtasks:
-				dump['subtasks'].append(t.serialize())
+		dump = dict(name=self.name, users=self.users, groups=self.groups, deadline=self.deadline, priority=self.priority, desc="\n".join(self.desc), subtasks=self.subtasks)
 		return dump
 
 class TokenParser:
 	project = None
-	last_context = None	
+	task = None
+	milestone = None
 
 	TOKENS = {
 		'@u:' : 'users',
@@ -99,7 +98,8 @@ class TokenParser:
 		'#' : 'project',
 		'>' : 'deadline',
 		'-' : 'tasks', 
-		'--' : 'subtasks'
+		'--' : 'subtasks',
+		'*': 'comment'
 	}
 
 	_PARSER = None
@@ -107,12 +107,18 @@ class TokenParser:
 	def __init__(self):
 		self.project = Project()
 
+	def regetattr(self, traversal):
+		attrs = traversal.split(".")
+		last_attr = self
+		for someattr in attrs:
+			last_attr = getattr(last_attr, someattr)
+
 	@property
 	def regex_parser(self):
 		if self._PARSER is not None:
 			return self._PARSER
 
-		TOKENS_REGEX='(@u:|@g:)\>(#|##)\!\-(\s+\-)\$'
+		TOKENS_REGEX='(@u:|@g:)\>(#|##)\!\*\-(\s+\-)\$'
 		REGEX = f'^([{TOKENS_REGEX}]+)\s*(.*)$'
 		self._PARSER = re.compile(REGEX)
 
@@ -135,23 +141,17 @@ class TokenParser:
 				# Markdown compatibility for subtasks
 				if token is None:
 					token = "subtasks"
+				elif token == "comment":
+					# comments are igored
+					continue
 
-			self.process(token, data)
+			token_fn = getattr(self, f"process_{token}")
+			if token != 'project' and self.project.name is None:
+				raise ValueError("Project needs to be defined first")
 
-		import pdb
-		pdb.set_trace()
-	
-		self.finalize()
+			token_fn(data)
+
 		return self.project.serialize()
-
-	def process(self, token, data):
-		if token is None:
-			return
-		token_fn = getattr(self, f"process_{token}")
-		if token != 'project' and self.project.name is None:
-			raise ValueError("Project needs to be defined first")
-
-		token_fn(data)
 	
 	def process_project(self, data):
 		if self.project.name is not None:
@@ -166,77 +166,59 @@ class TokenParser:
 		self.project.name = name.strip()
 		self.project.namespace = namespace.strip()
 
-		self.last_context = "project"
-
-	def _process_usergroup_scope(self, user_or_group):
-		if self.milestone:
-			scope = self.task if self.task else self.milestone
+	def _update_active_context_node(self, nodeproperty, value):
+		obj = None
+		if self.task is not None:
+			activenode="task"
+			obj = self.project.milestones[self.milestone].tasks[self.task]
+		elif self.milestone is not None:
+			activenode ="milestone"
+			obj = self.project.milestones[self.milestone]
 		else:
-			scope = self.project
+			activenode="project"
+			obj = self.project
 
-		scopelist = scope.users if user_or_group == "users" else scope.groups
-		return scopelist
+		current_value = getattr(obj, nodeproperty)
+		if isinstance(current_value, list):
+			current_value.append(value)
+		else:
+			setattr(obj, nodeproperty, value)
 
 	def process_users(self, data):
-		userlist = self._process_usergroup_scope("users")
-		userlist.append(data)
+		self._update_active_context_node("users", data)
 
 	def process_groups(self, data):
-		grouplist = self._process_usergroup_scope("groups")
-		grouplist.append(data)
+		self._update_active_context_node("groups", data)
 
 	def process_tasks(self, data):
-		if not self.milestone: # create default milestone
-			self.process_milestones("Project Tasks")
+		if self.milestone is None:
+			self.process_milestones("Project tasks")
 
-		if self.task:		
-			self.milestone.tasks.append(self.task)
-			del self.task
-		self.task = Task(name=data)
+		value = Task(name=data)
+		self.task = None # reset
+		self._update_active_context_node("tasks", value)
+		self.task = len(self.project.milestones[-1].tasks) -1  # get last reference
 
 	def process_subtasks(self, data):
-		if not self.task:
+		if self.task is None:
 			raise ValueError("A subtask needs a parent task")
-		print("-=", self.task, data)
-		self.task.subtasks.append(Task(name=data, subtasks=None))
+		self._update_active_context_node("subtasks", data)
 
 	def process_milestones(self, data):
-		if self.milestone:
-			self.project.milestones.append(self.milestone)
-			del self.milestone
+		newmilestone = Milestone(name=data)
+		self.project.milestones.append(newmilestone)
 
-		self.milestone = Milestone(name=data)
-	
-	def _scope_deadline_priority_desc(self):	
-		if self.task:
-			objname = "task"
-		elif self.milestone:
-			objname = "milestone"
-		else:
-			objname = "project"
-
-		return objname
+		self.milestone = len(self.project.milestones) - 1
+		self.task = None
 
 	def process_deadline(self, data):
-		objname = self._scope_deadline_priority_desc()
-		setattr(getattr(self, objname), "deadline", data)
+		self._update_active_context_node("deadline", data)
 
 	def process_priority(self, data):
-		objname = self._scope_deadline_priority_desc()
-		setattr(getattr(self, objname), "priority", data)
+		self._update_active_context_node("priority", data)
 
 	def process_desc(self, data):
-		objname = self._scope_deadline_priority_desc()
-		desc = getattr(getattr(self, objname), "desc")
-		desc.append(data)
-
-	def finalize(self):
-		if not self.project.name:
-			pass
-		elif self.task:
-			self.milestone.tasks.append(self.task)
-
-		self.project.milestones.append(self.milestone)
+		self._update_active_context_node("desc", data)
 
 tokenparser = TokenParser()
 project_dump = tokenparser.run_parser(workdoc)
