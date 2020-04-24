ae009cd334ce4fee5d53d4f96b0db8b2e9565df1
acpmasquerade <acpmasquerade@gmail.com>
2020-04-24 13:24:16 +0545

    some basic js functions and save feature

--- parsers/workfile_flask/sample.py
+++ parsers/workfile_flask/sample.py
@@ -5,6 +5,7 @@ import json
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import abort, make_response
 
 from parsers.workfile_py import workfileparser
 
@@ -16,20 +17,50 @@ def get_payload_and_project():
 	else:
 		payload = open("examples/01.basicsample.work").read()
 
-	parser = workfileparser.TokenParser()
-	project = parser.parse_string(payload)
+	try:
+		parser = workfileparser.TokenParser()
+		project = parser.parse_string(payload)
 
-	project_json = json.dumps(project, indent=4)
+		project_json = json.dumps(project, indent=4)
+	except Exception as exc:
+		return False, payload, None, None, str(exc)
 
-	return payload, project, project_json
+	return True, payload, project, project_json, "OK"
 
 @app.route('/', methods=['POST', 'GET'])
 def workfile_html():
-	payload, project, project_json = get_payload_and_project()
+	status, payload, project, project_json, msg = get_payload_and_project()
+	if status is False:
+		return make_response(msg, 400)
 	return render_template("./workfile.html", project=project, rawdata=payload)
 
 @app.route('/parse/', methods=['POST'])
 def workfile_parser():
-	payload, project, project_json = get_payload_and_project()
+	status, payload, project, project_json, msg = get_payload_and_project()
+	if status is False:
+		return make_response(msg, 400)
 	return render_template("./parsed_output.html", project=project, payload=payload, project_json=project_json)
 
+
+@app.route('/save/', methods=['POST'])
+def save():
+	filename = request.form.get('filename').split("/")[-1]
+	contents = request.form.get('content')
+
+	ff=open(f"parsers/workfile_flask/saved_files/{filename}.workfile", "w")
+	ff.write(contents)
+	ff.close()
+
+	return make_response(f"File saved as {filename}")
+	
+
+@app.route('/load/', methods=['POST'])
+def load_file():
+	filename = request.form.get('filename').split("/")[-1]
+
+	ff=open(f"parsers/workfile_flask/saved_files/{filename}.workfile", "r")
+	dump = ff.read()
+	ff.close()
+
+	return make_response(dump)
+	
